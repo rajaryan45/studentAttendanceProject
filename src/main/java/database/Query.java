e65e32d5104303a20cd04668b5e0e72dc147f5b8
Raj Aryan Behera <rajaryanbehera@gmail.com>
2023-05-14 08:36:11 +0530

    Teacher_GET_SET_V1

--- src/main/java/database/Query.java
+++ src/main/java/database/Query.java
@@ -1,5 +1,10 @@
 package database;
 
 public class Query {
-	protected static final String SELECT_STUDENT_ADMN_ID = "SELECT * FROM student WHERE student_admn_id = ?"; 
+	
+	//Query's are Strored here 
+	
+	protected static final String SELECT_STUDENT_ADMN_ID = "SELECT * FROM student WHERE student_admn_id = ?";
+	protected static final String SELECT_TEACHER_ID = "SELECT * FROM teacher WHERE teacher_id = ?";
+	
 }
--- src/main/java/teacher/info/Teacher.java
+++ src/main/java/teacher/info/Teacher.java
@@ -1,5 +1,8 @@
 package teacher.info;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Teacher {
 	private int teacherId;
 	private String teacherName;
@@ -9,5 +12,78 @@ public class Teacher {
 	private String phoneNumber;
 	private String currentClass;
 	private String currentSection;
+	
+	public Teacher() {
+		super();
+	}
+	
+	public Teacher(ResultSet rset){
+		try {
+			System.out.println("Teacher");
+			this.setTeacherId(Integer.parseInt(rset.getString("teacher_id")));
+			this.setTeacherName(rset.getString("teacher_name"));
+			System.out.println("teacher name : " + rset.getString("teacher_name"));
+			this.setSubject(rset.getString("subject"));
+			this.setDob(rset.getString("dob"));
+			this.setAddress(rset.getString("address"));
+			this.setPhoneNumber(rset.getString("phone_number"));
+			this.setCurrentClass(rset.getString("current_class"));
+			this.setCurrentSection(rset.getString("current_section"));
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public int getTeacherId() {
+		return teacherId;
+	}
+	public void setTeacherId(int teacherId) {
+		this.teacherId = teacherId;
+	}
+	public String getTeacherName() {
+		return teacherName;
+	}
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getCurrentClass() {
+		return currentClass;
+	}
+	public void setCurrentClass(String currentClass) {
+		this.currentClass = currentClass;
+	}
+	public String getCurrentSection() {
+		return currentSection;
+	}
+	public void setCurrentSection(String currentSection) {
+		this.currentSection = currentSection;
+	}
+	
+	
 	
 }
